de25626b2862d4f205ff69799eb496c12f725dbf
Dandav2923 <daniele.emili23@gmail.com>
2022-02-21 15:43:56 +0100

    trains

--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
-class TrainTableSeeder extends Seeder
+use App\Trains;
+class TrainsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,6 +11,6 @@ class TrainTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        
     }
 }
